5e067c9b18b8b09020a5c1786d4968dd4394ce12
v24dc <v.sharkovskiy@gmail.com>
2018-04-19 22:49:28 +0300

    Simple webpack config.

---
+++ webpack-4-simple/webpack.config.js
@@ -0,0 +1,21 @@
+// webpack v4 config
+const path = require('path');
+
+module.exports = {
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+}
