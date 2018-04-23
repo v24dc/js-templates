0c4b2a07a3e388479164cfa0e5cceae66cf4e070
v24dc <v.sharkovskiy@gmail.com>
2018-04-23 14:11:51 +0300

    Mini-css-extract-plugin, postcss, autoprefixer.
    Signed-off-by: v24dc <v.sharkovskiy@gmail.com>

--- webpack-4-simple/webpack.config.js
+++ webpack-4-simple/webpack.config.js
@@ -1,13 +1,14 @@
 // webpack v4 config
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const WebpackMd5Hash = require('webpack-md5-hash');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
   entry: { main: './src/index.js' },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
+    filename: '[name].[chunkhash].js'
   },
   module: {
     rules: [
@@ -20,20 +21,28 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader']
-        })
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
       }
     ]
   },
   plugins: [
-    new ExtractTextPlugin({ filename: 'style.css' }),
+    // new ExtractTextPlugin(
+    //   {filename: 'style.[hash].css', disable: false, allChunks: true }
+    // ),
+    new MiniCssExtractPlugin({
+      filename: 'style.[contenthash].css'
+    }),
     new HtmlWebpackPlugin({
       inject: false,
       hash: true,
       template: './src/index.html',
       filename: 'index.html'
-    })
+    }),
+    new WebpackMd5Hash()
   ]
 };
