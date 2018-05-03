bc74438597efdda0791b14055110221410e6c102
v24dc <v.sharkovskiy@gmail.com>
2018-05-03 15:13:21 +0300

    Add react.
    Signed-off-by: v24dc <v.sharkovskiy@gmail.com>

--- webpack-4-react/src/index.js
+++ webpack-4-react/src/index.js
@@ -1,5 +1,9 @@
 import './styles/main.scss';
-console.log('Hello! Test!');
+import App from './components/App';
+import React from 'react';
+import { render } from 'react-dom';
+
+render(<App />, document.getElementById('app'));
 
 if (module.hot) {
   console.log(module);
