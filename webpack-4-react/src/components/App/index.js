32ff25026ad771452e269dab1448354f0cf53896
v24dc <v.sharkovskiy@gmail.com>
2018-06-27 20:44:37 +0300

    Update deps to latest versions.
    Signed-off-by: v24dc <v.sharkovskiy@gmail.com>

--- webpack-4-react/src/components/App/index.js
+++ webpack-4-react/src/components/App/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 export default class App extends React.Component {
   render() {
-    return <div>I am App!</div>;
+    return (
+      <div>
+        <h3>I am App!</h3>
+        <p>
+          Deps updated 27/06/2018
+        </p>
+      </div>);
   }
 }
